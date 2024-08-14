99f56b01c9a0b73abac825ea40d2a75462f5dda3
tgarratt <tomagarratt@yahoo.co.uk>
2024-08-14 08:38:05 +0100

    feat: add static files

--- run_router_django/settings.py
+++ run_router_django/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -126,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
